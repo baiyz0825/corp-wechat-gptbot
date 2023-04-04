24d29e991e6d54c5cebe73034fdc5a3c47a0813e
baiyz0825 <byz0825@outlook.com>
2023-04-05 04:32:47 +0800

    修改帮助菜单

--- xconst/const.go
+++ xconst/const.go
@@ -31,7 +31,7 @@ const (
 func GetDefaultNoticeMenu() string {
 	// 默认 提示消息
 	return `这里是帮助菜单（如下是支持的菜单，以下不存在默认不进行处理）：
-@@help：帮助菜单 -> 例子：@help
+@help：帮助菜单 -> 例子：@help
 @clear：清除聊天上下文 -> 例子：@clear
 @image: 根据你的描述生成图片 -> 例子：@image 生成一只黑色的猫
 @prompt-set：设置默认角色描述 -> 例子：@@prompt-set 你是一个资深的程序员`
